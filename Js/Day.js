10703ec250c8c137a2627cb03caa812864c5f8b8
funshin <65276376+funshin@users.noreply.github.com>
2020-07-07 13:22:10 +0530

    Create Day.js

---
+++ Js/Day.js
@@ -0,0 +1,19 @@
+
+<!--
+
+var weekdays = new Array(7);
+weekdays[6] = "Sunday";
+weekdays[0] = "Monday";
+weekdays[1] = "Tuesday";
+weekdays[2] = "Wednesday";
+weekdays[3] = "Thursday";
+weekdays[4] = "Friday";
+weekdays[5] = "Saturday";
+
+var current_date = new Date();
+
+weekday_value = current_date.getDay();
+
+document.write(weekdays[weekday_value]);
+
+//-->
